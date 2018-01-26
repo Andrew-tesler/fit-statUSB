ee7ccd37618c073cdf31ad5ce820b01cd3f4471c
Andrew Tesler <andrey.tesler@compulab.co.il>
2018-01-26 10:11:26 +0200

    Removed previusly commented out lines

--- myTimers.c
+++ myTimers.c
@@ -46,13 +46,10 @@ void initTimers(int red,int green,int blue) {
     if (blue >= 0 & blue <= 255)
         Blue = blue;
 
-    if (Red == 0) {
-           GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, LED_R); /// Input power off the RED led
-       }
+    if (Red == 0) {                                                             // Fix the LED red Powering ON when not in use
+        GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, LED_R);     // Input power off the RED led
+    }
 
-    //    currentRGBColor[0] = Red;
-    //    currentRGBColor[1] = Green;
-    //    currentRGBColor[2] = Blue;
 
     GPIO_setAsPeripheralModuleFunctionOutputPin(LED_PORT,LED_R + LED_G + LED_B);// Set the RGB LED GPIO to alternative function to power the LEDS directly from timer
 
@@ -75,7 +72,7 @@ void initTimers(int red,int green,int blue) {
     initCompareParamcc1.compareRegister 		= TIMER_A_CAPTURECOMPARE_REGISTER_1;
     initCompareParamcc1.compareInterruptEnable	= TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE;
     initCompareParamcc1.compareOutputMode       = TIMER_A_OUTPUTMODE_TOGGLE_SET;    // ARDUMSP
-//        initCompareParamcc1.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
+    //        initCompareParamcc1.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
     initCompareParamcc1.compareValue			= Red;
     //
     // Blue
@@ -83,7 +80,7 @@ void initTimers(int red,int green,int blue) {
     initCompareParamcc2.compareRegister 		= TIMER_A_CAPTURECOMPARE_REGISTER_2;
     initCompareParamcc2.compareInterruptEnable	= TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE;
     initCompareParamcc2.compareOutputMode       = TIMER_A_OUTPUTMODE_TOGGLE_SET;    // ARDUMSP
-//        initCompareParamcc2.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
+    //        initCompareParamcc2.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
     initCompareParamcc2.compareValue			= Blue;
 
     // Red
@@ -91,7 +88,7 @@ void initTimers(int red,int green,int blue) {
     initCompareParamcc3.compareRegister 		= TIMER_A_CAPTURECOMPARE_REGISTER_3;
     initCompareParamcc3.compareInterruptEnable	= TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE;
     initCompareParamcc3.compareOutputMode       = TIMER_A_OUTPUTMODE_TOGGLE_SET;    // ARDUMSP
-//        initCompareParamcc3.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
+    //        initCompareParamcc3.compareOutputMode       = TIMER_A_OUTPUTMODE_SET_RESET;     // FITSTATUSB
     initCompareParamcc3.compareValue			= Green;
     //
     Timer_A_initCompareMode(TIMER_A0_BASE, &initCompareParamcc1);
@@ -108,15 +105,6 @@ void initTimers(int red,int green,int blue) {
     //Timer_A_disableInterrupt(TIMER_A0_BASE);
 
     Timer_A_startCounter(TIMER_A0_BASE,TIMER_A_UP_MODE);
-
-
-
-
-
-
-
-
-
 }
 
 // Fade the leds from given color to given color by the time given
@@ -142,9 +130,6 @@ void initfadeClock() {
 // calculate  the time for the fade function
 void updateFadeTime(uint32_t totalTime) {
 
-    //    fadeTime = totalTime/fadeColorStep;                                         // Calculate fade step
-
-
     // initfade(0,0,0,0,0,0,0);
     fadeTimer = totalTime;
     initFade(colorsNumber);
@@ -163,7 +148,7 @@ void initFade(uint8_t colorNum) {
     colorsNumber = colorNum;
     colortick = 0;                                                                                                                          // Clear time tick number
     fadeArrayLocation = 0;
-//    fadeArrayDirection = 0;
+    //    fadeArrayDirection = 0;
     initTimers(colorSeq[0][0], colorSeq[0][1], colorSeq[0][2]);                                                                             // Start the LED with the first color
     colorLocation = 0;
     currentRGBColor[0] = colorSeq[0][0];
@@ -226,284 +211,6 @@ __interrupt void timer_ISRB0 (void) {
         direction = !direction;
         colorLocation=0;
     }
-    // Calculate only when needed  TODO add code later
 
-
-
-
-
-
-    //initTimers(colorSeq[colortick][0], colorSeq[0][1], colorSeq[0][2]);
-    //initTimers(currentRGBColor[0] + colorFadeDiff[0] ,currentRGBColor[1] + colorFadeDiff[1],currentRGBColor[2] + colorFadeDiff[2]);
-    //    initTimers(currentRGBColor[0],currentRGBColor[1],currentRGBColor[2]);
-    //colorLocation // Location of current color
     Timer_B_clearCaptureCompareInterrupt(TIMER_B0_BASE, TIMER_B_CAPTURECOMPARE_REGISTER_0);
-
-
-
-    /*
-
-    for (smallerCounter = 0 ; smallerCounter < 3 ; smallerCounter++) {
-        if (colorFadeDiff[0][smallerCounter] > 0) {
-            fadeDirection[smallerCounter] = 1;  // Count up
-        }
-        else {
-            fadeDirection[smallerCounter] = 0; // Count down
-        }
-
-
-        switch (fadeDirection[smallerCounter]) {
-
-        case 1: {
-            if (colortick[smallerCounter] >= fadeTimeMs[0][smallerCounter]) {
-                currentRGBColor[smallerCounter]++;
-                colortick[smallerCounter] = 0;
-            }
-
-
-        }
-        case 0: {
-            if (colortick[smallerCounter] >= fadeTimeMs[0][smallerCounter]) {
-                currentRGBColor[smallerCounter] = currentRGBColor[smallerCounter]-1;
-                colortick[smallerCounter] = 0;
-            }
-        }
-
-        }
-
-
-    }
-    Timer_A_stop(TIMER_A0_BASE);
-    initTimers(currentRGBColor[0],currentRGBColor[1],currentRGBColor[2]);
-     */
-
-    //    initTimers(currentRGBColor[0],currentRGBColor[1],currentRGBColor[2]);
-    //
-    //    if (timerTick >= 100) {
-    //        currentRGBColor[0] = currentRGBColor[0] + 1 ;
-    //        timerTick=0;
-    //        Timer_A_stop(TIMER_A0_BASE);
-    //        initTimers(currentRGBColor[0],currentRGBColor[1],currentRGBColor[2]);
-    //    }
-    // if ()
-    //    colorCounter[MAX_SEQ_COLORS][3];
-    //    currentRGBColor
-    //    fadeTimeMs
-    //    colorSeq
-
-
-    // Stop Timer A and update only when needed
-
-
-
-
-    /*
-
-
-    //GPIO_setAsPeripheralModuleFunctionOutputPin(LED_PORT,LED_R + LED_G + LED_B);    // Set GPIO Pin alternative function to blink directly from timer
-    initTimers(0x01,0x01,timer);
-
-
-
-
-    //
-    //
-    ////
-    //    fadeTickGlobal = fadeTime/2;                                                  // TODO the 255 number will have to be the color diference of each led
-
-    if (fadeTick <= fadeTimer) {                                                           // If the fade tick smaller that the calculted global
-        //
-        fadeTick++;
-
-    }
-    ////    // FasdeTick havent reached incriment the counter
-    else {
-        fadeTick=0;
-
-        switch(direction) {
-        // Counting UP
-        case 0:
-            if (timer<fadeColorStep) {
-                timer++;
-            }
-            else {
-                direction=1;
-            }
-            break;
-            // Counting Down
-        case 1:
-            if (timer>0) {
-                timer--;
-            }
-            else {
-                direction=0;
-            }
-
-            break;
-        }
-        //
-    }
-    ////
-    //                                                                 // Update the timer
-
-
-    //
-    ////    GPIO_setAsOutputPin(LED_PORT, LED_B);
-    ////        GPIO_toggleOutputOnPin( LED_PORT, LED_B );
-    //
-    //
-     *
-     */
-
-
 }
-//
-//#pragma vector=TIMER0_B1_VECTOR
-//__interrupt void timer_ISRB1 (void) {
-//    //GPIO_toggleOutputOnPin( LED_PORT, LED_G );
-//    Timer_B_clearCaptureCompareInterrupt(TIMER_B0_BASE, TIMER_B_CAPTURECOMPARE_REGISTER_1);
-//
-//
-//}
-
-//#pragma vector=TIMER0_A1_VECTOR
-//__interrupt void timer_ISR2 (void)
-//{
-//    GPIO_toggleOutputOnPin( GPIO_PORT_P4, GPIO_PIN7 );
-//
-//}
-//
-//#pragma vector=TIMER0_A0_VECTOR
-//__interrupt void TIMER1_A0_ISR (void)
-//
-//{
-//
-//    //GPIO_toggleOutputOnPin( GPIO_PORT_P6, GPIO_PIN0 );
-//    //**************************************************************************
-//    // 4. Timer ISR and vector
-//    //**************************************************************************
-//    switch( __even_in_range( TA0IV, TA0IV_TAIFG )) {
-//    case TA0IV_NONE: break;                 // (0x00) None
-//    case TA0IV_TACCR1:                      // (0x02) CCR1 IFG
-//        // GPIO_toggleOutputOnPin(LED_PORT, LED_G);
-//        //    	 GPIO_toggleOutputOnPin( GPIO_PORT_P6, GPIO_PIN0 );
-//        //          _no_operation();
-//        break;
-//    case TA0IV_TACCR2:                      // (0x04) CCR2 IFG
-//        //    	 GPIO_setOutputHighOnPin( LED_PORT, LED_R );
-//        // GPIO_toggleOutputOnPin( LED_PORT, LED_R );
-//
-//        //          _no_operation();
-//        break;
-//    case TA0IV_TACCR3:                      // (0x06) CCR3 IFG
-//
-//        //          _no_operation();
-//        break;
-//    case TA0IV_TACCR4:                      // (0x08) CCR4 IFG
-//        _no_operation();
-//        break;
-//    case TA0IV_5: break;                    // (0x0A) Reserved
-//    case TA0IV_6: break;                    // (0x0C) Reserved
-//    case TA0IV_TAIFG:                       // (0x0E) TA0IFG - TAR overflow
-//        // Toggle LED2 (Green) LED on/off
-//
-//        _no_operation();
-//        break;
-//    default: _never_executed();
-//    }
-//}
-
-////***** Defines ***************************************************************
-//
-//
-////***** Global Variables ******************************************************
-//
-//
-////*****************************************************************************
-//// Initialize Timer
-////*****************************************************************************
-//void initTimers(void)
-//{
-//    //*************************************************************************
-//    // 1. Setup Timer (TAR, TACTL)
-//    //    TimerA in Up mode using ACLK
-//    //    Toggle LED1 (Red) on/off every second directly from the timers CCR2 output pin
-//    //    Toggle LED2 (Green) on/off every second using TAxIFG
-//    //*************************************************************************
-//    Timer_A_initUpModeParam initUpParam = { 0 };
-//        initUpParam.clockSource = TIMER_A_CLOCKSOURCE_ACLK;                       // Use ACLK (slower clock)
-//        initUpParam.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;           // Input clock = ACLK / 1 = 32KHz
-//        initUpParam.timerPeriod = 0xFFFF / 2;                                     // Period (0xFFFF/2):  8000 / 32Khz = 1 second
-//        initUpParam.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;    // Enable TAR -> 0 interrupt
-//        initUpParam.captureCompareInterruptEnable_CCR0_CCIE =
-//            TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;                                  // No CCR0 interrupt
-//        initUpParam.timerClear = TIMER_A_DO_CLEAR;                                // Clear TAR & clock divider
-//        initUpParam.startTimer = false;                                           // Don't start the timer, yet
-//    Timer_A_initUpMode( TIMER_A0_BASE, &initUpParam );
-//
-//    //*************************************************************************
-//    // 2. Setup Capture & Compare features
-//    //*************************************************************************
-//    Timer_A_initCompareModeParam initCcr2Param = { 0 };
-//        initCcr2Param.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_2;        // Use CCR2 for compare
-//        initCcr2Param.compareInterruptEnable =
-//            TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;                             // Since directly driving LED, interrup not req'd
-//        initCcr2Param.compareOutputMode = TIMER_A_OUTPUTMODE_TOGGLE_RESET;        // Toggle provides a 1 sec period based on CCR0 and CCR2 values
-//        initCcr2Param.compareValue = 0x100;                                      // Compare value: 4000 = 1/2 second
-//    Timer_A_initCompareMode( TIMER_A0_BASE, &initCcr2Param );
-//
-//
-//
-//    //*************************************************************************
-//    // 3. Clear/enable interrupt flags and start timer
-//    //*************************************************************************
-//    Timer_A_clearTimerInterrupt( TIMER_A0_BASE );                                    // Clear TAxIFG
-//
-//    //Initiaze compare mode
-//    Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_2 + TIMER_A_CAPTURECOMPARE_REGISTER_3);
-//
-//
-//
-//
-//    Timer_A_startCounter(
-//        TIMER_A0_BASE,
-//        TIMER_A_UP_MODE
-//    );
-//}
-//
-////*****************************************************************************
-//// Interrupt Service Routine
-////*****************************************************************************
-//#pragma vector=TIMER0_A0_VECTOR
-//__interrupt void timer_ISR (void)
-//{
-//    //**************************************************************************
-//    // 4. Timer ISR and vector
-//    //**************************************************************************
-//    switch( __even_in_range( TA0IV, TA0IV_TAIFG )) {
-//     case TA0IV_NONE: break;                 // (0x00) None
-//     case TA0IV_TACCR1:                      // (0x02) CCR1 IFG
-//          _no_operation();
-//           break;
-//     case TA0IV_TACCR2:                      // (0x04) CCR2 IFG
-//    	 GPIO_toggleOutputOnPin( GPIO_PORT_P4, GPIO_PIN7 );
-////          _no_operation();
-//           break;
-//     case TA0IV_TACCR3:                      // (0x06) CCR3 IFG
-//    	 GPIO_toggleOutputOnPin( GPIO_PORT_P6, GPIO_PIN0 );
-////          _no_operation();
-//           break;
-//     case TA0IV_TACCR4:                      // (0x08) CCR4 IFG
-//          _no_operation();
-//           break;
-//     case TA0IV_5: break;                    // (0x0A) Reserved
-//     case TA0IV_6: break;                    // (0x0C) Reserved
-//     case TA0IV_TAIFG:                       // (0x0E) TA0IFG - TAR overflow
-//          // Toggle LED2 (Green) LED on/off
-//
-//    	 _no_operation();
-//          break;
-//     default: _never_executed();
-//    }
-//}
-
